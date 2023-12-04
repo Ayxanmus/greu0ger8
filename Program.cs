d63c9abfb3fe32609a2d0723e0fc255c7ebaaf03
Ayxanmus <152673157+Ayxanmus@users.noreply.github.com>
2023-12-04 09:39:22 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿
-int i = Convert.ToInt32(Console.ReadLine());
+﻿int i = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
-while (i != 0) 
+while (i <= 100)
 {
 
-    sum = sum + i; 
+    i++;
+    sum++;
     i = Convert.ToInt32(Console.ReadLine());
 }
 
 Console.WriteLine(sum);
+
