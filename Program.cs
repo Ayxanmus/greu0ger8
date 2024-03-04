2176ae4e9382d9963f1aabb95be9b4315386f7f5
Ayxanmus <152673157+Ayxanmus@users.noreply.github.com>
2024-03-04 09:35:42 +0500

    проект

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
-﻿int sum = 0;
-int[] numbers = new int[5]{ 1, 2, 3, 4, 5, };
-for (int i = 0; i < 5; i++)
-sum = sum + numbers[i];
-Console.WriteLine(sum);
+﻿Console.WriteLine("Введите пароль от аккаунта ");
+string user = Console.ReadLine();
+string password = "12345";
+if (password == user)
+{
+    Console.WriteLine("passsword true ");
+}
+else
+{
+    Console.WriteLine(" password wrong, try again ");
+    string bad = Console.ReadLine();
+    string bat = "12345";
+    if (password == user)
+    {
+        Console.WriteLine("password true ");
+    }
+    
 
+
+}
+
+
+
+Console.WriteLine("опишите свою деятельность ");
+string play  = Console.ReadLine();
+
+Console.WriteLine("Пройдете еше тест ?");
+string name = Console.ReadLine();
+string bear = "да ";
+if (name == bear   )
+{
+    
+
+}
+Console.WriteLine("напишите нужно ли проводить такие тесты ");
+string beard = Console.ReadLine();
